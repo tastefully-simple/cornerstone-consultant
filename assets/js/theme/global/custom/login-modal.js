b8f6738fb1619de81d1af48f15c013a7069b8927
Andresa Martins <amartins@razoyo.com>
2023-03-16 04:35:52 -0300

    TST-835 — Show login modal with a different message after 1 hour
    TST-835 — Show login modal with a different message after 1 hour

--- assets/js/theme/global/custom/login-modal.js
+++ assets/js/theme/global/custom/login-modal.js
@@ -6,6 +6,9 @@ export default function (status) {
         return;
     }
 
+    // Set last time the user loaded a page + 1 hour
+    window.localStorage.setItem('consultant-timeout', new Date().getTime() + 3600000);
+
     const loginModal = modalFactory('#loginModal')[0];
     const content = $('#loginModal .login-modal-form:first');
     const $loginModalTrigger = $('#loginModal--trigger');
@@ -13,4 +16,14 @@ export default function (status) {
         loginModal.open();
         loginModal.updateContent(content);
     });
+
+    // Verify if the page has been inactive for more than 1 hour. If it is, show login popup
+    setTimeout(() => {
+        if (new Date().getTime() > window.localStorage.getItem('consultant-timeout')) {
+            loginModal.open();
+            loginModal.updateContent(content);
+            $('#sso_login_message').hide();
+            $('#sso_login_message_inactive').show();
+        }
+    }, 30000);
 }
